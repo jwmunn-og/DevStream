ba53e9135fb0d27232a08b163edc34db019133e8
jwmunn <contact@justinmunn.co>
2017-03-14 09:44:47 -0700

    User model refactor #2.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def enrolled_in?(course)
-    enrolled_courses = erollments.collect do |enrollment|
-      enrollment.course
-    end
-
+    enrolled_courses = erollments.collect(&:course)
     return enrolled_courses.include?(course)
   end
   
