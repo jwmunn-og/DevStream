b05dce3fa3a5520ca79d4f09530e8ef1e5e41efe
jwmunn <contact@justinmunn.co>
2017-03-15 01:08:34 -0700

    Added draggable sorting to sections.

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_autorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -11,19 +12,34 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'Updated!'
+  end
+
   private
-    def require_autorized_for_current_course
-      if current_course.user != current_user
-        return render text: 'Unauthorized', status: :unauthorized
-      end
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render text: 'Unauthorized', status: :unauthorized
     end
-    
-    helper_method :current_course
-    def current_course
-      @current_course ||= Course.find(params[:course_id])
+  end
+
+  def current_section
+    current_section ||= Section.find(params[:id])
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      return render text: 'Unauthorized', status: :unauthorized
     end
-    
-    def section_params
-      params.require(:section).permit(:title)
-    end
+  end
+  
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
+  
+  def section_params
+    params.require(:section).permit(:title, :row_order_position)
+  end
 end
