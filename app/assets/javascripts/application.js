9c96d4467654108a318cbb80c1508c6a7d9970aa
jwmunn <contact@justinmunn.co>
2017-03-20 17:36:54 -0400

    Moved section#new action into modal on course page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery-ui/widgets/sortable
 //= require foundation
 //= require turbolinks
 //= require_tree .
-
-$(function(){ $(document).foundation(); });
